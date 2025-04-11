a47335fd5c27d13d4c11f00b3d1bc51665d5c386
mevyona <mevyona@gmail.com>
2025-04-11 16:05:48 +0200

    Fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,17 +1,6 @@
-function fish_prompt
-    set_color green
-    echo -n (whoami) "@" (hostname) " " (pwd) " > "
-    set_color normal
-end
-
 if status is-interactive
     set fish_greeting
 
-end
-
-starship init fish | source
-if test -f ~/.cache/ags/user/generated/terminal/sequences.txt
-    cat ~/.cache/ags/user/generated/terminal/sequences.txt
 end
 
 alias reload='source ~/.config/fish/config.fish'
@@ -23,11 +12,66 @@ alias pmr='sudo pacman -Rns'
 alias update='sudo pacman -Syu'
 alias clean='sudo pacman -Rns $(pacman -Qdtq)'
 
-alias lsl='ls -l'
-alias lsa='ls -a'
+alias lsl='ls -lF'
+alias lsa='ls -aF'
 
 alias sn='shutdown now'
 alias r='reboot'
 
 alias c='clear'
 alias ff='fastfetch'
+
+# Configuration des couleurs selon la palette du prompt
+# Couleurs extraites du fish_prompt.fish
+set -l main_color E2C0FC
+set -l accent_color B4BEFE
+set -l secondary_accent CBA6F7
+set -l error_color F38BA8
+set -l success_color A6E3A1
+set -l info_color 89DCEB
+set -l warn_color FAB387
+
+# Couleurs pour fish
+set -U fish_color_normal normal
+set -U fish_color_command $main_color
+set -U fish_color_keyword $secondary_accent
+set -U fish_color_quote $warn_color
+set -U fish_color_redirection $info_color
+set -U fish_color_end $success_color
+set -U fish_color_error $error_color
+set -U fish_color_param $accent_color
+set -U fish_color_option $accent_color
+set -U fish_color_comment $warn_color --italics
+set -U fish_color_selection --background=$secondary_accent
+set -U fish_color_search_match --background=$secondary_accent
+set -U fish_color_operator $info_color
+set -U fish_color_escape $info_color
+set -U fish_color_autosuggestion 6c7086
+set -U fish_color_cancel $error_color
+
+# Couleurs pour le pager (comme dans la complétion)
+set -U fish_pager_color_progress $info_color
+set -U fish_pager_color_prefix $main_color --bold
+set -U fish_pager_color_completion $accent_color
+set -U fish_pager_color_description $warn_color
+set -U fish_pager_color_selected_background --background=$secondary_accent
+
+# Couleurs pour ls et autres commandes
+# S'assurer que les couleurs sont correctement appliquées
+# 1. Définir LS_COLORS avec notre palette personnalisée
+set -gx LS_COLORS "di=01;38;2;180;166;247:fi=38;2;205;214;244:ln=01;38;2;137;220;235:so=01;38;2;249;226;175:pi=01;38;2;249;226;175:ex=01;38;2;166;227;161:bd=01;38;2;245;194;231:cd=01;38;2;245;194;231:su=01;38;2;243;139;168:sg=01;38;2;243;139;168:tw=01;38;2;166;227;161:ow=01;38;2;166;227;161:*.md=38;2;137;220;235:*.txt=38;2;137;220;235:*.sh=01;38;2;166;227;161:*.py=01;38;2;166;227;161"
+# 2. Forcer les couleurs dans ls (créer des alias avec --color=always)
+alias ls='ls -F --color=always'
+alias dir='dir -F --color=always'
+alias vdir='vdir -F --color=always'
+
+# Configuration pour grep
+set -gx GREP_COLORS "mt=01;38;2;226;192;252:fn=01;38;2;180;166;247:ln=01;38;2;137;220;235:bn=01;38;2;249;226;175:se=01;38;2;166;227;161"
+
+# Configuration pour less (utilisé pour les pages man)
+set -Ux LESS_TERMCAP_md (printf "\e[01;38;2;226;192;252m")  # Titres (Bold)
+set -Ux LESS_TERMCAP_me (printf "\e[0m")                    # Fin de mode
+set -Ux LESS_TERMCAP_so (printf "\e[01;38;2;137;220;235m")  # Info/search highlight
+set -Ux LESS_TERMCAP_se (printf "\e[0m")                    # Fin de info/search
+set -Ux LESS_TERMCAP_us (printf "\e[01;38;2;180;166;247m")  # Soulignement
+set -Ux LESS_TERMCAP_ue (printf "\e[0m")                    # Fin de soulignement
---
+++ config/fish/functions/fish_prompt.fish
@@ -0,0 +1,149 @@
+function fish_prompt
+    set -l last_status $status
+    set -l cmd_duration $CMD_DURATION
+    
+    set -l bg_main 1E1E2E
+    set -l bg_lighter 313244
+    set -l main_color E2C0FC
+    set -l accent_color B4BEFE
+    set -l secondary_accent CBA6F7
+    set -l error_color F38BA8
+    set -l success_color A6E3A1
+    set -l info_color 89DCEB
+    set -l warn_color FAB387
+    set -l path_color E2C0FC
+    set -l user_host_color CBA6F7
+    
+    set -l separator ""
+    set -l git_symbol "󰊢"
+    set -l python_symbol ""
+    set -l node_symbol ""
+    set -l time_symbol "󰥔"t
+    set -l folder_symbol "󰝰"
+    set -l error_symbol ""
+    set -l root_symbol "󰯄"
+    set -l user_symbol "󰣇"
+    set -l machine_symbol "󰌢"
+    set -l prompt_char "→"
+    set -l right_separator ""
+    set -l arch_symbol "󰣇"
+    
+    function __segment
+        set -l bg $argv[1]
+        set -l fg $argv[2]
+        set -l content $argv[3]
+        set -l next_bg $argv[4]
+        
+        set_color -b $bg $fg
+        echo -n "$content"
+        
+        if test -n "$next_bg"
+            set_color -b $next_bg $bg
+            echo -n "$separator"
+        else
+            set_color normal
+            set_color $bg
+            echo -n "$separator"
+        end
+        set_color normal
+    end
+    
+    function __right_segment
+        set -l bg $argv[1]
+        set -l fg $argv[2]
+        set -l content $argv[3]
+        
+        set_color -b $bg $fg
+        echo -n "$right_separator$content"
+        set_color normal
+    end
+    
+    echo
+    
+    __segment $main_color $bg_main " $arch_symbol " $bg_lighter
+    
+    __segment $user_host_color $bg_main " "(whoami)" " $bg_lighter
+    
+    __segment $path_color $bg_main " $folder_symbol "(prompt_pwd)" " $info_color
+    
+    if command -sq git; and git rev-parse --is-inside-work-tree >/dev/null 2>&1
+        set -l git_branch (git rev-parse --abbrev-ref HEAD 2>/dev/null)
+        set -l git_status (git status --porcelain 2>/dev/null)
+        set -l git_color $success_color
+        set -l git_state ""
+        
+        if test -n "$git_status"
+            set git_color $warn_color
+            set -l changes (count $git_status)
+            set git_state " $changes"
+        end
+        
+        set -l ahead_behind (git rev-list --count --left-right @{upstream}...HEAD 2>/dev/null)
+        if test $status -eq 0
+            echo $ahead_behind | read behind ahead
+            test $behind -gt 0; and set git_state "$git_state "
+            test $ahead -gt 0; and set git_state "$git_state "
+        end
+        
+        __segment $git_color $bg_main " $git_symbol $git_branch $git_state " $secondary_accent
+    end
+    
+    if set -q VIRTUAL_ENV
+        __segment $secondary_accent $bg_main " $python_symbol "(basename $VIRTUAL_ENV)" "
+    end
+    
+    set -l right_prompt ""
+    
+    if test $cmd_duration -gt 2000
+        __segment $git_color $bg_main "$git_symbol $git_branch$git_state" $accent_color
+    end
+    
+    if set -q VIRTUAL_ENV
+        set -l venv_name (basename $VIRTUAL_ENV)
+        __segment $accent_color $bg_main "$python_symbol $venv_name" $warn_color
+    end
+    
+    if test -e package.json
+        set -l node_version (node -v | string replace -r '^v' '')
+        __segment $warn_color $bg_main "$node_symbol $node_version"
+    end
+    
+    set -l right_prompt ""
+    
+    if test $cmd_duration -gt 2000
+        set -l duration (math $cmd_duration / 1000)
+        set right_prompt "$right_prompt$(__right_segment $info_color $bg_main "$time_symbol $duration s")"
+    end
+    
+    set -l jobs_count (jobs -p | wc -l)
+    if test $jobs_count -gt 0
+        set right_prompt "$right_prompt$(__right_segment $info_color $bg_main "󱜯 $jobs_count")"
+    end
+    
+    if test $last_status -ne 0
+        set right_prompt "$right_prompt$(__right_segment $error_color $bg_main "$error_symbol $last_status")"
+    end
+    
+    set right_prompt "$right_prompt$(__right_segment $main_color $bg_main)"
+    
+    set -l term_width (tput cols)
+    set -l prompt_width (string length (string replace -a '\e[[\d;]*m' '' "$right_prompt"))
+    set -l padding_width (math "$term_width - $prompt_width")
+    
+    printf "%*s%s\n" $padding_width "" "$right_prompt"
+    
+    if test (id -u) -eq 0
+        set_color $error_color
+        echo -n "$prompt_char "
+    else
+        if test $last_status -eq 0
+            set_color $main_color
+            echo -n "$prompt_char "
+        else
+            set_color $error_color
+            echo -n "$prompt_char "
+        end
+    end
+    
+    set_color normal
+end
